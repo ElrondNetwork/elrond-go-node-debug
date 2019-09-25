5eba1b26bb4e15134daf081dc04ec77d5d3b8611
Robert Sasu <sasu.robert@gmail.com>
2019-09-25 10:41:31 +0300

    create a tx processor node which can run transactions.

---
+++ cmd/debugWithRestApi/main.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"runtime"
+	"runtime/debug"
+	"sync"
+	"syscall"
+
+	debugVMFacade "github.com/ElrondNetwork/elrond-go-node-debug/facade"
+	"github.com/ElrondNetwork/elrond-go/cmd/node/factory"
+	"github.com/ElrondNetwork/elrond-go/config"
+	"github.com/ElrondNetwork/elrond-go/core"
+	"github.com/ElrondNetwork/elrond-go/core/logger"
+	"github.com/ElrondNetwork/elrond-go/data/state"
+	"github.com/ElrondNetwork/elrond-go/data/state/addressConverters"
+	factoryState "github.com/ElrondNetwork/elrond-go/data/state/factory"
+	"github.com/ElrondNetwork/elrond-go/facade"
+	"github.com/ElrondNetwork/elrond-go/node/external"
+	"github.com/ElrondNetwork/elrond-go/process/factory/shard"
+	"github.com/ElrondNetwork/elrond-go/process/smartContract"
+	"github.com/ElrondNetwork/elrond-go/sharding"
+	"github.com/ElrondNetwork/elrond-go/statusHandler"
+	"github.com/urfave/cli"
+)
+
+var (
+	nodeHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}
+   {{if len .Authors}}
+AUTHOR:
+   {{range .Authors}}{{ . }}{{end}}
+   {{end}}{{if .Commands}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}
+VERSION:
+   {{.Version}}
+   {{end}}
+`
+)
+
+// restApiPort defines a flag for port on which the rest API will start on
+var restApiPort = cli.StringFlag{
+	Name:  "rest-api-port",
+	Usage: "The port on which the rest API will start on",
+	Value: "8080",
+}
+
+// configurationFile defines a flag for the path to the main toml configuration file
+var configurationFile = cli.StringFlag{
+	Name:  "config",
+	Usage: "The main configuration file to load",
+	Value: "./config/config.toml",
+}
+
+func main() {
+	log := logger.DefaultLogger()
+
+	app := cli.NewApp()
+	cli.AppHelpTemplate = nodeHelpTemplate
+	app.Name = "Elrond Node CLI App"
+	app.Version = fmt.Sprintf("%s/%s/%s-%s", "debug", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	app.Usage = "This is the entry point for starting a new Elrond node - the app will start after the genesis timestamp"
+	app.Flags = []cli.Flag{
+		restApiPort,
+		configurationFile,
+	}
+	app.Authors = []cli.Author{
+		{
+			Name:  "The Elrond Team",
+			Email: "contact@elrond.com",
+		},
+	}
+
+	//TODO: The next line should be removed when the write in batches is done
+	// set the maximum allowed OS threads (not go routines) which can run in the same time (the default is 10000)
+	debug.SetMaxThreads(100000)
+
+	app.Action = func(c *cli.Context) error {
+		return startDebugNode(c, log)
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+
+}
+
+func startDebugNode(ctx *cli.Context, log *logger.Logger) error {
+	stop := make(chan bool, 1)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	log.Info(fmt.Sprintf("Starting node"))
+	log.Info(fmt.Sprintf("Process ID: %d\n", os.Getpid()))
+
+	generalConfig, err := loadMainConfig(ctx.GlobalString(configurationFile.Name), log)
+	if err != nil {
+		fmt.Println("error loading generalConfig " + err.Error())
+		return err
+	}
+
+	var workingDir = ""
+	workingDir, err = os.Getwd()
+	if err != nil {
+		log.LogIfError(err)
+		workingDir = ""
+	}
+
+	uniqueDBFolder := filepath.Join(
+		workingDir,
+		"defaultDBPath",
+		fmt.Sprintf("%s_%d", "0", 0),
+		fmt.Sprintf("%s_%s", "0", 0))
+
+	coreArgs := factory.NewCoreComponentsFactoryArgs(generalConfig, uniqueDBFolder)
+	coreComponents, err := factory.CoreComponentsFactory(coreArgs)
+	if err != nil {
+		fmt.Println("error creating core components " + err.Error())
+		return err
+	}
+
+	addressConverter, err := addressConverters.NewPlainAddressConverter(generalConfig.Address.Length, generalConfig.Address.Prefix)
+	if err != nil {
+		fmt.Println("could not create address converter: " + err.Error())
+		return err
+	}
+
+	shardCoordinator, err := sharding.NewMultiShardCoordinator(1, 0)
+	if err != nil {
+		return err
+	}
+
+	accountFactory, err := factoryState.NewAccountFactoryCreator(shardCoordinator)
+	if err != nil {
+		fmt.Println("could not create account factory: " + err.Error())
+		return err
+	}
+
+	accountsAdapter, err := state.NewAccountsDB(coreComponents.Trie, coreComponents.Hasher, coreComponents.Marshalizer, accountFactory)
+	if err != nil {
+		fmt.Println("could not create accounts adapter: " + err.Error())
+		return err
+	}
+
+	statusMetrics := statusHandler.NewStatusMetrics()
+	apiResolver, err := createApiResolver(
+		accountsAdapter,
+		addressConverter,
+		statusMetrics,
+	)
+	if err != nil {
+		fmt.Println("error instantiating api resolver " + err.Error())
+		return err
+	}
+
+	ef := debugVMFacade.NewDebugVMFacade(apiResolver, true)
+
+	efConfig := &config.FacadeConfig{
+		RestApiPort: ctx.GlobalString(restApiPort.Name),
+	}
+
+	ef.SetLogger(log)
+	ef.SetConfig(efConfig)
+
+	wg := sync.WaitGroup{}
+	go ef.StartBackgroundServices(&wg)
+	wg.Wait()
+
+	log.Info("Bootstrapping node....")
+	err = ef.StartNode()
+	if err != nil {
+		log.Error("starting node failed", err.Error())
+		return err
+	}
+
+	go func() {
+		<-sigs
+		log.Info("terminating at user's signal...")
+		stop <- true
+	}()
+
+	log.Info("Application is now running...")
+	<-stop
+
+	return nil
+}
+
+func createApiResolver(accounts state.AccountsAdapter, converter state.AddressConverter, statusMetrics external.StatusMetricsHandler) (facade.ApiResolver, error) {
+	vmFactory, err := shard.NewVMContainerFactory(accounts, converter)
+	if err != nil {
+		return nil, err
+	}
+
+	vmContainer, err := vmFactory.Create()
+	if err != nil {
+		return nil, err
+	}
+
+	scDataGetter, err := smartContract.NewSCDataGetter(vmContainer)
+	if err != nil {
+		return nil, err
+	}
+
+	return external.NewNodeApiResolver(scDataGetter, statusMetrics)
+}
+
+func loadMainConfig(filepath string, log *logger.Logger) (*config.Config, error) {
+	cfg := &config.Config{}
+	err := core.LoadTomlFile(cfg, filepath, log)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
---
+++ facade/nodeFacade.go
@@ -0,0 +1,180 @@
+package facade
+
+import (
+	"fmt"
+	"github.com/ElrondNetwork/elrond-go/node/heartbeat"
+	"strconv"
+	"sync"
+
+	"github.com/ElrondNetwork/elrond-go/api"
+	"github.com/ElrondNetwork/elrond-go/config"
+	"github.com/ElrondNetwork/elrond-go/core/logger"
+	"github.com/ElrondNetwork/elrond-go/core/statistics"
+	baseFacade "github.com/ElrondNetwork/elrond-go/facade"
+	"github.com/ElrondNetwork/elrond-go/node/external"
+	"github.com/ElrondNetwork/elrond-go/ntp"
+)
+
+// DefaultRestPort is the default port the REST API will start on if not specified
+const DefaultRestPort = "8080"
+
+// DefaultRestPortOff is the default value that should be passed if it is desired
+//  to start the node without a REST endpoint available
+const DefaultRestPortOff = "off"
+
+// ElrondNodeFacade represents a facade for grouping the functionality for node, transaction and address
+type DebugVMFacade struct {
+	apiResolver            baseFacade.ApiResolver
+	syncer                 ntp.SyncTimer
+	log                    *logger.Logger
+	tpsBenchmark           *statistics.TpsBenchmark
+	config                 *config.FacadeConfig
+	restAPIServerDebugMode bool
+}
+
+func (ef *DebugVMFacade) GetCurrentPublicKey() string {
+	return ""
+}
+
+func (ef *DebugVMFacade) GetHeartbeats() ([]heartbeat.PubKeyHeartbeat, error) {
+	return nil, nil
+}
+
+// NewElrondNodeFacade creates a new Facade with a NodeWrapper
+func NewDebugVMFacade(apiResolver baseFacade.ApiResolver, restAPIServerDebugMode bool) *DebugVMFacade {
+	if apiResolver == nil || apiResolver.IsInterfaceNil() {
+		return nil
+	}
+
+	return &DebugVMFacade{
+		apiResolver:            apiResolver,
+		restAPIServerDebugMode: restAPIServerDebugMode,
+	}
+}
+
+// SetLogger sets the current logger
+func (ef *DebugVMFacade) SetLogger(log *logger.Logger) {
+	ef.log = log
+}
+
+// SetSyncer sets the current syncer
+func (ef *DebugVMFacade) SetSyncer(syncer ntp.SyncTimer) {
+	ef.syncer = syncer
+}
+
+// SetTpsBenchmark sets the tps benchmark handler
+func (ef *DebugVMFacade) SetTpsBenchmark(tpsBenchmark *statistics.TpsBenchmark) {
+	ef.tpsBenchmark = tpsBenchmark
+}
+
+// TpsBenchmark returns the tps benchmark handler
+func (ef *DebugVMFacade) TpsBenchmark() *statistics.TpsBenchmark {
+	return ef.tpsBenchmark
+}
+
+// SetConfig sets the configuration options for the facade
+func (ef *DebugVMFacade) SetConfig(facadeConfig *config.FacadeConfig) {
+	ef.config = facadeConfig
+}
+
+// StartNode starts the underlying node
+func (ef *DebugVMFacade) StartNode() error {
+	return nil
+}
+
+// StopNode stops the underlying node
+func (ef *DebugVMFacade) StopNode() error {
+	return nil
+}
+
+// StartBackgroundServices starts all background services needed for the correct functionality of the node
+func (ef *DebugVMFacade) StartBackgroundServices(wg *sync.WaitGroup) {
+	wg.Add(1)
+	go ef.startRest(wg)
+}
+
+// IsNodeRunning gets if the underlying node is running
+func (ef *DebugVMFacade) IsNodeRunning() bool {
+	return true
+}
+
+// RestAPIServerDebugMode return true is debug mode for Rest API is enabled
+func (ef *DebugVMFacade) RestAPIServerDebugMode() bool {
+	return ef.restAPIServerDebugMode
+}
+
+// RestApiPort returns the port on which the api should start on, based on the config file provided.
+// The API will start on the DefaultRestPort value unless a correct value is passed or
+//  the value is explicitly set to off, in which case it will not start at all
+func (ef *DebugVMFacade) RestApiPort() string {
+	if ef.config == nil {
+		return DefaultRestPort
+	}
+	if ef.config.RestApiPort == "" {
+		return DefaultRestPort
+	}
+	if ef.config.RestApiPort == DefaultRestPortOff {
+		return DefaultRestPortOff
+	}
+
+	_, err := strconv.ParseInt(ef.config.RestApiPort, 10, 32)
+	if err != nil {
+		return DefaultRestPort
+	}
+
+	return ef.config.RestApiPort
+}
+
+// PrometheusMonitoring returns if prometheus is enabled for monitoring by the flag
+func (ef *DebugVMFacade) PrometheusMonitoring() bool {
+	return ef.config.Prometheus
+}
+
+// PrometheusJoinURL will return the join URL from server.toml
+func (ef *DebugVMFacade) PrometheusJoinURL() string {
+	return ef.config.PrometheusJoinURL
+}
+
+// PrometheusNetworkID will return the NetworkID from config.toml or the flag
+func (ef *DebugVMFacade) PrometheusNetworkID() string {
+	return ef.config.PrometheusJobName
+}
+
+func (ef *DebugVMFacade) startRest(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	switch ef.RestApiPort() {
+	case DefaultRestPortOff:
+		ef.log.Info(fmt.Sprintf("Web server is off"))
+		break
+	default:
+		ef.log.Info("Starting web server...")
+		err := api.Start(ef)
+		if err != nil {
+			ef.log.Error("Could not start webserver", err.Error())
+		}
+	}
+}
+
+// StatusMetrics will return the node's status metrics
+func (ef *DebugVMFacade) StatusMetrics() external.StatusMetricsHandler {
+	return ef.apiResolver.StatusMetrics()
+}
+
+// GetVmValue retrieves data from existing SC trie
+func (ef *DebugVMFacade) GetVmValue(address string, funcName string, argsBuff ...[]byte) ([]byte, error) {
+	return ef.apiResolver.GetVmValue(address, funcName, argsBuff...)
+}
+
+// PprofEnabled returns if profiling mode should be active or not on the application
+func (ef *DebugVMFacade) PprofEnabled() bool {
+	return ef.config.PprofEnabled
+}
+
+// IsInterfaceNil returns true if there is no value under the interface
+func (ef *DebugVMFacade) IsInterfaceNil() bool {
+	if ef == nil {
+		return true
+	}
+	return false
+}
